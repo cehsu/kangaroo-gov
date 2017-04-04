83f88ca0280c89da868ac4757672995f40ac1ede
Claire Hsu <clairehsu86@gmail.com>
2017-04-03 17:44:09 -0700

    Update

--- root/test/testGetWinner.js
+++ root/test/testGetWinner.js
@@ -3,13 +3,13 @@ var assert = require('assert');
 
 describe('getWinner Function', function() {
   describe('Test with a candidate array and a ballot matrix', function() {
-    it('should return John Doe when these are ["John Doe", "Jane Smith", "Jane Austen"],[[1 2 3], [2 1 3], [2 3 1], [1 2 3], [3 1 2]]', function() {
-      assert.equal(getWinner(["John Doe", "Jane Smith", "Jane Austen"],[[1 2 3], [2 1 3], [2 3 1], [1 2 3], [3 1 2]]), "John Doe");
+    it('should return John Doe when these are ["John Doe", "Jane Smith", "Jane Austen"],[[1, 2, 3], [2, 1, 3], [2, 3, 1], [1, 2, 3], [3, 1, 2]]', function() {
+      assert.equal(getWinner(["John Doe", "Jane Smith", "Jane Austen"],[[1, 2, 3], [2, 1, 3], [2, 3, 1], [1, 2, 3], [3, 1, 2]]), "John Doe");
     });
   });
   describe('Test with a tied candidates', function() {
-    it('should return tie when these are ["John Doe", "Jane Smith", "Jane Austen"],[[1 2 3], [2 1 3],', function() {
-      assert.equal(getWinner(["John Doe", "Jane Smith", "Jane Austen"],[[1 2 3], [2 1 3], [2 3 1]]), "tie"));
+    it('should return tie when these are ["John Doe", "Jane Smith", "Jane Austen"],[[1, 2, 3], [2, 1, 3],[2, 3, 1]]', function() {
+      assert.equal(getWinner(["John Doe", "Jane Smith", "Jane Austen"],[[1, 2, 3], [2, 1, 3], [2, 3, 1]]), "tie"));
     });
   });
 });
